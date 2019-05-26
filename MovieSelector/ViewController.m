383186aaff99a126d207b6091cc4ecb5ebdeffff
ajlongcoy21 <ajlongcoy21@gmail.com>
2019-05-26 13:55:13 -0500

    added assets to project and created the intro screen to the app

--- MovieSelector/ViewController.m
+++ MovieSelector/ViewController.m
@@ -14,9 +14,30 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // Create a flash animation to help the user see and want to click the button
+    
+    CABasicAnimation *flash = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    flash.duration = 0.75;
+    flash.fromValue = @1.0;
+    flash.toValue = @0.1;
+    flash.autoreverses = true;
+    flash.repeatCount = 1000;
+    flash.timingFunction = [CAMediaTimingFunction functionWithName:@"easeInEaseOut"];
+    
+    // Add flash animaiton to the button
+    
+    [_movieSelectorButton.layer addAnimation:flash forKey:nil];
+
+}
+
+- (IBAction)startMovieSelection:(id)sender
+{
+    
 }
 
 
